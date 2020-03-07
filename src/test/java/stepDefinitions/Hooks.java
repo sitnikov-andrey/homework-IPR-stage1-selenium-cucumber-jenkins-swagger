019ed97481ead16099bbd5405339a661181a8e04
Sitnik54 <sitnik54@gmail.com>
2020-03-07 17:24:23 +0300

    rename demo -> calculator. add Hooks and Examples in calculator.feature

---
+++ src/test/java/stepDefinitions/Hooks.java
@@ -0,0 +1,16 @@
+package stepDefinitions;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class Hooks {
+    @Before
+    public void prepareData() {
+        System.out.println("Старт теста");
+    }
+
+    @After
+    public void clearData() {
+        System.out.println("Конец теста");
+    }
+}
